1463800cec445f0e79129fc70f9919a40d05eb17
siddharth gairola <siddharth.42602@stu.upes.ac.in>
2026-09-25 01:13:55 +0530

    Add files via upload

---
+++ day 7a.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+int year;
+
+printf("Enter a year: ");
+scanf("%d", &year);
+
+if (year % 400 == 0)
+{
+printf("Leap year");
+}
+else if (year % 4 == 0)
+{
+printf("Leap year");
+}
+else if (year % 100 == 0)
+{
+printf("Not a leap year");
+}
+else
+{
+printf("Not a leap year");
+}
+
+return 0;
+}
